8177f3ac3c7fe76d02efadf9c941e69fb1f8c96f
Ksenia616 <zxkseniya@mail.ru>
2025-08-15 11:10:44 +0300

    ///

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,7 @@ function Home({ onStart }) {
       <h1 className="text-xl font-light mb-4">
         Привет! Я твой бьюти-ассистент Marvel Cosmetics
       </h1>
-      <img
-        src={logo}
-        alt="Marvel Logo"
-        className="w-48 mb-6 animate-pulse-silver"
-      />
+      <img src={logo} alt="Marvel Logo" className="w-48 mb-6 animate-pulse-silver" />
       <button
         onClick={onStart}
         className="bg-white text-black px-6 py-3 rounded hover:bg-gray-300 transition"
